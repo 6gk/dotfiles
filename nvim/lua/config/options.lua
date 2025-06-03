7e0d180bdc9ad9b8d310159e28f4197769eb230f
epithet <>
2025-06-03 17:00:02 -0500

    added my current neovim config

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+local o = vim.opt
+
+o.nu = true
+o.hls = true
+o.is = true
+o.ic = true
+o.scs = true
+o.et = true
+o.swf = false
+o.bk = false
+o.sw = 4
+o.sts = 4
---
+++ nvim/lua/config/plugins.lua
@@ -0,0 +1,42 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+
+require("lazy").setup({
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons"
+        },
+        config = function()
+            require("lualine").setup({})
+        end
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000,
+        config = function()
+            require("kanagawa").setup({
+                commentStyle = { italic = true },
+                keywordStyle = { italic = true },
+                statementStyle = { bold = true },
+            })
+            vim.cmd("colorscheme kanagawa")
+        end
+    },
+})
